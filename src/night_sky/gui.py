9375426d8d9673030c4dfadaeaa4d4876dba81a9
Daniel Kucera <dany411.kucera@gmail.com>
2025-06-20 18:25:10 +0200

    update

--- src/night_sky/gui.py
+++ src/night_sky/gui.py
@@ -1,3 +1,24 @@
 import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton
+
+class Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        self.setWindowTitle("My Form")
+        self.edit = QLineEdit("Write my name here..")
+        self.button = QPushButton("Show Greetings")
+        
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+
+
+if __name__ == '__main__':
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec())
