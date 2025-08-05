41ce100813b0cb14ff92eca7b217343e075a4c49
Daniel Kucera <dkucera@unyp.cz>
2025-08-05 17:58:24 +0200

    update

--- nightsky/src/night_sky/render.py
+++ nightsky/src/night_sky/render.py
@@ -4,7 +4,7 @@ import pyqtgraph.opengl as gl
 from PyQt6.QtWidgets import QApplication
 import sys
 
-def spherical_to_cartesian(alt, az, radius=1000):
+def spherical_to_cartesian(alt, az, radius=999):
     """Convert spherical coordinates to Cartesian coordinates."""
     theta = np.pi / 2 - alt
     x = radius * np.sin(theta) * np.cos(az)
@@ -53,14 +53,16 @@ def stars(star_data):
     for star in star_data:
         alt, az = star.alt, star.az
         x, y, z = spherical_to_cartesian(alt, az)
-        sphere = gl.GLScatterPlotItem(pos=[(x, y, z)], size= (-star.magnitude + 7)*1.2, color=(0.5, 0.5, 1, 0.5))
+        sphere = gl.GLScatterPlotItem(pos=[(x, y, z)], size= (-star.magnitude + 7)*1.2, color=(0.5, 0.5, 1, 0))
         sphere.setGLOptions('opaque')
         list_of_stars.append(sphere)
         print(alt, az, x, y, z)
     return list_of_stars
 
-def coordinate_polar_axes(r=1000):
-    """Create coordinate axes."""
+def alt_polar_axes(r=1000):
     axis_alt = [gl.GLLinePlotItem(pos=np.array([[r*np.cos(np.deg2rad(angle))*float(np.cos(np.pi/72 * a)), r*np.cos(np.deg2rad(angle))*float(np.sin(np.pi/72 * a)), r*np.sin(np.deg2rad(angle))] for a in np.arange(145)]), color=(0.5, 0.5, 0.5, 0.5), width=2) for angle in np.arange(-180,180,10)]
-    axis_az = [gl.GLLinePlotItem(pos=np.array([[r*np.cos(np.deg2rad(angle))*float(np.cos(np.pi/72 * a)), r*np.cos(np.deg2rad(angle))*float(np.sin(np.pi/72 * a)), r*np.sin(np.deg2rad(angle))] for a in np.arange(145)]), color=(0.5, 0.5, 0.5, 0.5), width=2) for angle in np.arange(-180,180,10)]
     return axis_alt
+
+def az_polar_axes(r=1000):
+    axis_az = [gl.GLLinePlotItem(pos=np.array([[r*np.sin(np.deg2rad(angle))*np.sin(np.pi/72*a), r*np.cos(np.deg2rad(angle))*np.sin(np.pi/72*a), r*np.cos(np.pi/72*a)] for a in np.arange(145)]), color=(0.5, 0.5, 0.5, 0.5), width=2) for angle in np.arange(0, 180, 10)]
+    return axis_az
